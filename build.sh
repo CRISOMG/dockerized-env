5bdf0e84661280f43b50d05f7b091be17b02df3e
Cristian <cristiancaraballo112@gmail>
2024-11-07 23:40:24 -0400

    improve build script with production mode arg

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-docker compose up --build -d;
-docker cp express-api:/api/node_modules ./express-api/api/node_modules;
-docker cp nuxt-app:/app/node_modules ./nuxt-app/app/node_modules;
+if [[ $1 == "production" ]]; then
+    NODE_ENV=production docker compose up --build -d;
+else
+    NODE_ENV=development docker compose up --build -d;
+fi
+
+sudo docker cp express-api:/api/node_modules ./express-api/api/node_modules;
+sudo docker cp nuxt-app:/app/node_modules ./nuxt-app/app/node_modules;
