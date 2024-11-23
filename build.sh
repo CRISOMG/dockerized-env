baf67744c284f229e06836f19924e78e9791f350
Cristian <cristiancaraballo112@gmail>
2024-11-23 16:23:43 -0400

    refactoring docker up flows

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ if [[ $1 == "production" ]]; then
     NODE_ENV=production docker compose up --build -d;
 else
     NODE_ENV=development docker compose up --build -d;
+    sudo docker cp express-api:/api/node_modules ./express-api/api/node_modules;
+    sudo docker cp express-api:/api/dist ./express-api/api/dist;
+    sudo docker cp nuxt-app:/app/node_modules ./nuxt-app/app/node_modules;
+    sudo docker cp nuxt-app:/app/.output ./nuxt-app/app/.output;
 fi
-
-sudo docker cp express-api:/api/node_modules ./express-api/api/node_modules;
-sudo docker cp nuxt-app:/app/node_modules ./nuxt-app/app/node_modules;
