27f7c7f1c4800665dd20a9e6cb55438ac6b7266b
Cristian <cristiancaraballo112@gmail>
2024-11-24 13:48:04 -0400

    fixing

--- git-clone.sh
+++ git-clone.sh
@@ -2,7 +2,6 @@
 # remember do "chmod +x git-clone.sh" before execute this script 
 # chmod +x git-clone.sh && ./git-clone.sh
 
-repo_slug="CRISOMG/dockerized-env.git"
 # ssh_priv_key="-----BEGIN OPENSSH PRIVATE KEY-----
 # TODO
 # -----END OPENSSH PRIVATE KEY-----"
@@ -12,9 +11,10 @@ repo_slug="CRISOMG/dockerized-env.git"
 # ssh-add ~/.ssh/omg-media;
 # git clone --recurse-submodules git@github.com:$repo_slug;
 
+repo_slug="CRISOMG/dockerized-env.git"
 github_personal_access_toke="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"
 repo_url="https://$github_personal_access_toke@github.com/$repo_slug"
 
 git clone $repo_url ./;
-git clone "https://$github_personal_access_toke@github.com/CRISOMG/onegroup-web.git" ./nuxt-app/app;
-git clone "https://$github_personal_access_toke@github.com/CRISOMG/onegroup-api.git" ./express-api/api;
+git clone "https://$github_personal_access_toke@github.com/CRISOMG/onegroup-web.git" ./nuxt-app;
+git clone "https://$github_personal_access_toke@github.com/CRISOMG/onegroup-api.git" ./express-api;
