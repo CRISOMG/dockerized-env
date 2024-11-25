d89ffdafb28bdec75a4df3c32c4d2ef4abd74d6f
Cristian <cristiancaraballo112@gmail>
2024-11-24 21:26:32 -0400

    refactoring

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 if [[ $1 == "production" ]]; then
-    NODE_ENV=production docker compose up --build -d;
+    NODE_ENV=production docker compose -f ./docker-compose.production.yml up --build -d;
 else
-    NODE_ENV=development docker compose up --build -d;
-    sudo docker cp express-api:/api/node_modules ./express-api/api/node_modules;
-    sudo docker cp express-api:/api/dist ./express-api/api/dist;
-    sudo docker cp nuxt-app:/app/node_modules ./nuxt-app/app/node_modules;
-    sudo docker cp nuxt-app:/app/.output ./nuxt-app/app/.output;
+    NODE_ENV=development docker compose -f ./docker-compose.development.yml up --build -d;
+    sudo docker cp express-api:/api/node_modules ./express-api/node_modules;
+    sudo docker cp express-api:/api/dist ./express-api/dist;
+    sudo docker cp nuxt-app:/app/node_modules ./nuxt-app/node_modules;
+    sudo docker cp nuxt-app:/app/.output ./nuxt-app/.output;
+
+    sudo chown -R $USER:$USER ./express-api/node_modules;
+    sudo chown -R $USER:$USER ./express-api/dist;
+    sudo chown -R $USER:$USER ./nuxt-app/node_modules;
+    sudo chown -R $USER:$USER ./nuxt-app/.output;
 fi
